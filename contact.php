d3d5e39fac6a2f4e5d363a5175d1ec3954e86e54
Bob93 <goossen.bob@gmail.com>
2016-03-30 15:56:11 +0200

    Added files I edited

---
+++ contact.php
@@ -0,0 +1,470 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="author" content="SemiColonWeb" />
+
+	<!-- Stylesheets
+	============================================= -->
+	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<link rel="stylesheet" href="css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+
+	<link rel="stylesheet" href="css/responsive.css" type="text/css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<!--[if lt IE 9]>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/plugins.js"></script>
+
+	<!-- Document Title
+	============================================= -->
+	<title>Contact - Commoth VOF</title>
+
+</head>
+
+<body class="stretched">
+
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+
+		<!-- Header
+		============================================= -->
+		<header id="header" class="full-header">
+
+			<div id="header-wrap">
+
+				<div class="container clearfix">
+
+					<div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+
+					<!-- Logo
+					============================================= -->
+					<div id="logo">
+						<a href="index.php" class="standard-logo" data-dark-logo="images/commoth-logo.png"><img src="images/commoth-logo.png" alt="Commoth Logo"></a>
+						<a href="index.php" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="images/logo@2x.png" alt="Canvas Logo"></a>
+					</div><!-- #logo end -->
+
+					<!-- Primary Navigation
+					============================================= -->
+					<nav id="primary-menu">
+
+						<ul>
+							<li><a href="index.php"><div>Home</div></a></li>
+							<li><a href="portfolio.php"><div>Portfolio</div></a></li>
+							<li><a href="aboutus.php"><div>Over ons</div></a></li>
+							<li class="current"><a href="contact.php"><div>Contact</div></a></li>
+						</ul>
+
+						<!-- Top Search
+						============================================= -->
+						<!--<div id="top-search">
+							<a href="#" id="top-search-trigger"><i class="icon-search3"></i><i class="icon-line-cross"></i></a>
+							<form action="search.html" method="get">
+								<input type="text" name="q" class="form-control" value="" placeholder="Type &amp; Hit Enter..">
+							</form>
+						</div><!-- #top-search end -->
+
+					</nav><!-- #primary-menu end -->
+
+				</div>
+
+			</div>
+
+		</header><!-- #header end -->
+
+		<!-- Content
+		============================================= -->
+		<section id="content">
+
+			<div class="content-wrap">
+
+				<div class="col_four_fourth center">
+					<span class="subtitle">
+						Welkom bij Commoth.
+						Wij zijn nog studenten die nog volop in de leer zijn.
+						Door onze ervaringen op school en buiten school bieden wij goede websites aan voor een goede prijs.
+						Wij passen ons aan naar de wensen van de klant en proberen hierdoor een zo goed mogelijk product
+						neer te zetten waar wij en de klant tevreden mee kunnen zijn.
+					</span>
+				</div>
+
+				<div class="container clearfix">
+
+					<div class="col_one_fourth nobottommargin">
+						<div class="feature-box fbox-center fbox-light fbox-effect nobottomborder">
+							<div class="fbox-icon">
+								<a href="#"><i class="i-alt noborder icon-shop"></i></a>
+							</div>
+							<h3>e-Commerce Solutions<span class="subtitle">Start your Own Shop today</span></h3>
+						</div>
+					</div>
+
+					<div class="col_one_fourth nobottommargin">
+						<div class="feature-box fbox-center fbox-light fbox-effect nobottomborder">
+							<div class="fbox-icon">
+								<a href="#"><i class="i-alt noborder icon-wallet"></i></a>
+							</div>
+							<h3>Easy Payment Options<span class="subtitle">Credit Cards &amp; PayPal Support</span></h3>
+						</div>
+					</div>
+
+					<div class="col_one_fourth nobottommargin">
+						<div class="feature-box fbox-center fbox-light fbox-effect nobottomborder">
+							<div class="fbox-icon">
+								<a href="#"><i class="i-alt noborder icon-megaphone"></i></a>
+							</div>
+							<h3>Instant Notifications<span class="subtitle">Realtime Email &amp; SMS Support</span></h3>
+						</div>
+					</div>
+
+					<div class="col_one_fourth nobottommargin col_last">
+						<div class="feature-box fbox-center fbox-light fbox-effect nobottomborder">
+							<div class="fbox-icon">
+								<a href="#"><i class="i-alt noborder icon-fire"></i></a>
+							</div>
+							<h3>Hot Offers Daily<span class="subtitle">Upto 50% Discounts</span></h3>
+						</div>
+					</div>
+
+					<div class="clear"></div><div class="line bottommargin-lg"></div>
+
+				</div>
+
+				<div class="container clearfix">
+
+					<div class="heading-block center">
+						<h3>Some of our <span>Featured</span> Works</h3>
+						<span>We have worked on some Awesome Projects that are worth boasting of.</span>
+					</div>
+
+					<div id="oc-portfolio" class="owl-carousel portfolio-carousel portfolio-nomargin">
+
+						<div class="oc-item">
+							<div class="iportfolio">
+								<div class="portfolio-image">
+									<a href="portfolio-single.html">
+										<img src="images/portfolio/4/1.jpg" alt="Open Imagination">
+									</a>
+									<div class="portfolio-overlay">
+										<a href="images/portfolio/full/1.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
+										<a href="portfolio-single.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
+									</div>
+								</div>
+								<div class="portfolio-desc">
+									<h3><a href="portfolio-single.html">Open Imagination</a></h3>
+									<span><a href="#">Media</a>, <a href="#">Icons</a></span>
+								</div>
+							</div>
+						</div>
+
+						<div class="oc-item">
+							<div class="iportfolio">
+								<div class="portfolio-image">
+									<a href="portfolio-single.html">
+										<img src="images/portfolio/4/2.jpg" alt="Locked Steel Gate">
+									</a>
+									<div class="portfolio-overlay">
+										<a href="images/portfolio/full/2.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
+										<a href="portfolio-single.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
+									</div>
+								</div>
+								<div class="portfolio-desc">
+									<h3><a href="portfolio-single.html">Locked Steel Gate</a></h3>
+									<span><a href="#">Illustrations</a></span>
+								</div>
+							</div>
+						</div>
+
+						<div class="oc-item">
+							<div class="iportfolio">
+								<div class="portfolio-image">
+									<a href="#">
+										<img src="images/portfolio/4/3.jpg" alt="Mac Sunglasses">
+									</a>
+									<div class="portfolio-overlay">
+										<a href="http://vimeo.com/89396394" class="left-icon" data-lightbox="iframe"><i class="icon-line-play"></i></a>
+										<a href="portfolio-single-video.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
+									</div>
+								</div>
+								<div class="portfolio-desc">
+									<h3><a href="portfolio-single-video.html">Mac Sunglasses</a></h3>
+									<span><a href="#">Graphics</a>, <a href="#">UI Elements</a></span>
+								</div>
+							</div>
+						</div>
+
+						<div class="oc-item">
+							<div class="iportfolio">
+								<div class="portfolio-image">
+									<a href="portfolio-single.html">
+										<img src="images/portfolio/4/5.jpg" alt="Console Activity">
+									</a>
+									<div class="portfolio-overlay">
+										<a href="images/portfolio/full/5.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
+										<a href="portfolio-single.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
+									</div>
+								</div>
+								<div class="portfolio-desc">
+									<h3><a href="portfolio-single.html">Console Activity</a></h3>
+									<span><a href="#">UI Elements</a>, <a href="#">Media</a></span>
+								</div>
+							</div>
+						</div>
+
+						<div class="oc-item">
+							<div class="iportfolio">
+								<div class="portfolio-image">
+									<a href="portfolio-single-video.html">
+										<img src="images/portfolio/4/7.jpg" alt="Backpack Contents">
+									</a>
+									<div class="portfolio-overlay">
+										<a href="http://www.youtube.com/watch?v=kuceVNBTJio" class="left-icon" data-lightbox="iframe"><i class="icon-line-play"></i></a>
+										<a href="portfolio-single-video.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
+									</div>
+								</div>
+								<div class="portfolio-desc">
+									<h3><a href="portfolio-single-video.html">Backpack Contents</a></h3>
+									<span><a href="#">UI Elements</a>, <a href="#">Icons</a></span>
+								</div>
+							</div>
+						</div>
+
+						<div class="oc-item">
+							<div class="iportfolio">
+								<div class="portfolio-image">
+									<a href="portfolio-single.html">
+										<img src="images/portfolio/4/8.jpg" alt="Sunset Bulb Glow">
+									</a>
+									<div class="portfolio-overlay">
+										<a href="images/portfolio/full/8.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
+										<a href="portfolio-single.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
+									</div>
+								</div>
+								<div class="portfolio-desc">
+									<h3><a href="portfolio-single.html">Sunset Bulb Glow</a></h3>
+									<span><a href="#">Graphics</a></span>
+								</div>
+							</div>
+						</div>
+
+						<div class="oc-item">
+							<div class="iportfolio">
+								<div class="portfolio-image">
+									<a href="portfolio-single-video.html">
+										<img src="images/portfolio/4/10.jpg" alt="Study Table">
+									</a>
+									<div class="portfolio-overlay">
+										<a href="http://vimeo.com/91973305" class="left-icon" data-lightbox="iframe"><i class="icon-line-play"></i></a>
+										<a href="portfolio-single-video.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
+									</div>
+								</div>
+								<div class="portfolio-desc">
+									<h3><a href="portfolio-single-video.html">Study Table</a></h3>
+									<span><a href="#">Graphics</a>, <a href="#">Media</a></span>
+								</div>
+							</div>
+						</div>
+
+						<div class="oc-item">
+							<div class="iportfolio">
+								<div class="portfolio-image">
+									<a href="portfolio-single.html">
+										<img src="images/portfolio/4/11.jpg" alt="Workspace Stuff">
+									</a>
+									<div class="portfolio-overlay">
+										<a href="images/portfolio/full/11.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
+										<a href="portfolio-single.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
+									</div>
+								</div>
+								<div class="portfolio-desc">
+									<h3><a href="portfolio-single.html">Workspace Stuff</a></h3>
+									<span><a href="#">Media</a>, <a href="#">Icons</a></span>
+								</div>
+							</div>
+						</div>
+
+					</div>
+
+					<script type="text/javascript">
+
+						jQuery(document).ready(function($) {
+
+							var ocPortfolio = $("#oc-portfolio");
+
+							ocPortfolio.owlCarousel({
+								margin: 1,
+								autoplay: true,
+								autoplayHoverPause: true,
+								dots: false,
+								nav: true,
+								navText : ['<i class="icon-angle-left"></i>','<i class="icon-angle-right"></i>'],
+								responsive:{
+									0:{ items:1 },
+									600:{ items:2 },
+									1000:{ items:3 },
+									1200:{ items:4 }
+								}
+							});
+
+						});
+
+					</script>
+
+				</div>
+
+			</div>
+
+		</section><!-- #content end -->
+
+		<!-- Footer
+		============================================= -->
+		<footer id="footer" class="dark">
+
+			<div class="container">
+
+				<!-- Footer Widgets
+				============================================= -->
+				<div class="footer-widgets-wrap clearfix">
+
+					<div class="col_one_third">
+
+							<div class="widget clearfix">
+
+								<img src="images/commoth-logo.png" alt="" class="footer-logo">
+
+								<p>Wij geloven in  <strong>simpele</strong>, <strong>creatieve</strong> &amp; <strong>flexibele</strong> design standaarden.</p>
+
+							</div>
+
+					</div>
+
+					<div class="col_one_third">
+
+						<div style="background: url('images/world-map.png') no-repeat center center; background-size: 100%;">
+							<address>
+								Schaapsdrift 76<br>
+								6824GT, Arnhem<br>
+							</address>
+							<abbr title="Phone Number"><strong>Phone 1:</strong></abbr> +31 6 8117 69 49<br>
+							<abbr title="Phone Number"><strong>Phone 2:</strong></abbr> +31 6 5359 22 11<br>
+							<abbr title="Email Address"><strong>Email:</strong></abbr> commothnet@gmail.com
+						</div>
+
+					</div>
+
+					<div class="col_one_third col_last">
+
+						<div class="widget clearfix" style="margin-bottom: -20px;">
+
+							<div class="row">
+
+								<div class="col-md-6 clearfix bottommargin-sm">
+									<a href="#" class="social-icon si-dark si-colored si-facebook nobottommargin" style="margin-right: 10px;">
+										<i class="icon-facebook"></i>
+										<i class="icon-facebook"></i>
+									</a>
+									<a href="#"><small style="display: block; margin-top: 3px;"><strong>Like us</strong><br>on Facebook</small></a>
+								</div>
+
+								<div class="col-md-6 clearfix bottommargin-sm">
+									<a href="#" class="social-icon si-dark si-colored si-twitter nobottommargin" style="margin-right: 10px;">
+										<i class="icon-twitter"></i>
+										<i class="icon-twitter"></i>
+									</a>
+									<a href="#"><small style="display: block; margin-top: 3px;"><strong>Follow us</strong><br>on Twitter</small></a>
+								</div>
+
+								<div class="col-md-6 clearfix bottommargin-sm">
+									<a href="#" class="social-icon si-dark si-colored si-linkedin nobottommargin" style="margin-right: 10px;">
+										<i class="icon-linkedin"></i>
+										<i class="icon-linkedin"></i>
+									</a>
+									<a href="#"><small style="display: block; margin-top: 3px;"><strong>Follow us</strong><br>on LinkedIn</small></a>
+								</div>
+
+								<div class="col-md-6 clearfix bottommargin-sm">
+									<a href="#" class="social-icon si-dark si-colored si-github nobottommargin" style="margin-right: 10px;">
+										<i class="icon-github"></i>
+										<i class="icon-github"></i>
+									</a>
+									<a href="#"><small style="display: block; margin-top: 3px;"><strong>Follow us</strong><br>on Github</small></a>
+								</div>
+
+							</div>
+
+						</div>
+
+					</div>
+
+				</div><!-- .footer-widgets-wrap end -->
+
+			</div>
+
+			<!-- Copyrights
+			============================================= -->
+			<div id="copyrights">
+
+				<div class="container clearfix">
+
+					<div class="col_half">
+						Copyrights &copy; <?php echo date('Y'); ?> All Rights Reserved by Commoth VOF.<br>
+						<!--<div class="copyright-links"><a href="#">Terms of Use</a> / <a href="#">Privacy Policy</a></div>-->
+					</div>
+
+					<div class="col_half col_last tright">
+						<div class="fright clearfix">
+							<a href="#" class="social-icon si-small si-borderless si-facebook">
+								<i class="icon-facebook"></i>
+								<i class="icon-facebook"></i>
+							</a>
+
+							<a href="#" class="social-icon si-small si-borderless si-twitter">
+								<i class="icon-twitter"></i>
+								<i class="icon-twitter"></i>
+							</a>
+
+							<a href="#" class="social-icon si-small si-borderless si-github">
+								<i class="icon-github"></i>
+								<i class="icon-github"></i>
+							</a>
+
+							<a href="#" class="social-icon si-small si-borderless si-linkedin">
+								<i class="icon-linkedin"></i>
+								<i class="icon-linkedin"></i>
+							</a>
+						</div>
+
+						<div class="clear"></div>
+
+						<i class="icon-envelope2"></i> commothnet@gmail.com <span class="middot">&middot;</span> <i class="icon-headphones"></i> +31 6 5359 22 11 <span class="middot">&middot;</span> <i class="icon-headphones"></i> +31 6 8117 69 49
+					</div>
+
+				</div>
+
+			</div><!-- #copyrights end -->
+
+		</footer><!-- #footer end -->
+
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="js/functions.js"></script>
+
+</body>
+</html>
